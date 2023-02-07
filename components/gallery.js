f52bc3500858bbd8c75219bd13dbc88a7f40ac9f
SalvasDev <salsanzmelc@gmail.com>
2023-02-06 20:40:51 -0600

    probando regrear los a en lugar de Link

--- components/gallery.js
+++ components/gallery.js
@@ -9,6 +9,7 @@ export default function Gallery({ typeGallery }) {
   
   return (
     <div className="gallery">
+      { console.log(foundGallery)}
       { 
         foundGallery.map(project => (
           <Thumbnail 
