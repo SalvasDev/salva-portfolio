5afecc7781a6789bbf308ce8c9b36aa3cc9c1373
SalvasDev <salsanzmelc@gmail.com>
2023-03-06 08:23:21 -0600

    corrigiendo metadata para vista previa del sitio web3

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import previewOdissy from '../public/img/PreviewOdissy.png'
+// import previewOdissy from '../public/img/PreviewOdissy.png'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>      
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-      <meta property="og:image" content='../public/img/PreviewOdissy.png' />
+      {/* <meta property="og:image" content='../public/img/PreviewOdissy.png' /> */}
 
       </Head>  
       <body>
