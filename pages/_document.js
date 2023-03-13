ddec4e01eba857b059f8c630bc9bb610ce70bbed
SalvasDev <salsanzmelc@gmail.com>
2023-03-13 07:31:04 -0600

    Probando la verificación de google console

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ export default function Document() {
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         {/* Muestra la imagen del home en redes sociales */}
         {/* Etiquetas para Facebook */}
+        <meta name='description' content='Odisy es una empresa dedicada al diseño y desarrollo web en la ciudad de Morelia. Somos especialistas en diseño y desarrollo web, páginas web, landing pageas e identidad corporativa como logotipos, diseño gráfico profesional, diseño editorial y creación de contenido para redes sociales.'/>         
+        <meta name="google-site-verification" content="lPCCUtxUlK8TtxzFrrV1Qk2skVI7Og6R_cdSNDhqqnc" />
         <meta property="og:url" content="https://www.byodisy.com" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Odisy web site" />
