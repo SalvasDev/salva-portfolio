79587c0f39e8242199b910e937de08de07501108
SalvasDev <salsanzmelc@gmail.com>
2023-03-12 20:01:56 -0600

    Probando metas imagen home 2

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,16 @@ export default function Document() {
       <Head>      
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       
-      {/* Muestra la imagen del home en redes sociales */}
-      <meta property="og:image" content="../public/img/imageHome.png" />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
+        {/* Muestra la imagen del home en redes sociales */}
+        {/* Etiquetas para Facebook */}
+        <meta property="og:url" content="https://www.byodisy.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Odisy web site" />
+        <meta property="og:description" content="Odisy es una empresa dedicada al diseño y desarrollo web en la ciudad de Morelia." />
+        <meta property="og:image" content="https://www.byodisy.com/public/images/imageHome.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1851" />
+        <meta property="og:image:height" content="917" />
       </Head>  
       <body>
         <Main />
