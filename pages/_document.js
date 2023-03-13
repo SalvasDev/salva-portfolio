a22dff92eccdc915632c3f1e8b32165a95393926
SalvasDev <salsanzmelc@gmail.com>
2023-03-12 19:33:36 -0600

    Probando meta de imagen home

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>      
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-      {/* <meta property="og:image" content='../public/img/PreviewOdissy.png' /> */}
-
+      
+      {/* Muestra la imagen del home en redes sociales */}
+      <meta property="og:image" content="../public/img/imageHome.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       </Head>  
       <body>
         <Main />
