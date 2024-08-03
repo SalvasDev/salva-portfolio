1c9a0175f14bcfc17825f0a4f73ae9bb33321e44
Salvador Sánchez <salsanzmelc@gmail.com>
2024-08-02 18:02:56 -0600

    Quitando pokeapi

--- utilities/dbProjects.js
+++ utilities/dbProjects.js
@@ -1,172 +1,165 @@
 export const dbProjects = [
-
   // ////////////////////////////////////////////////////////////////////////////////
   // Database for web
   {
-    id: '101',
+    id: "101",
     area: "web",
     name: "Mountain",
     designed: "",
     developed: "Salvador Sánchez",
     thumbnail: "/img/mntn/thumb-mntn.png",
-    pics:
-      [
-        '/img/mntn/mntn-laptop.png',
-        '/img/mntn/mntn1.png',
-        '/img/mntn/mntn2.png',
-        '/img/mntn/mntn3.png',
-        '/img/mntn/mntn4.png'
-
-      ],
-    description: "<p><strong>Mountain</strong> es una landing page experimental desarrollada para demostrar el manejo del código de programación <br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, CSS3, JavaScript, SASS y la libreria de React. <br/> <br/> La barra lateral de navegación permite un desplazamiento más fluido y rápido en las versiones de escritorio.</p>",
-    code: 'https://github.com/SalvasDev/mntn',
-    demo: 'https://mntn-sal.netlify.app/'
+    pics: [
+      "/img/mntn/mntn-laptop.png",
+      "/img/mntn/mntn1.png",
+      "/img/mntn/mntn2.png",
+      "/img/mntn/mntn3.png",
+      "/img/mntn/mntn4.png",
+    ],
+    description:
+      "<p><strong>Mountain</strong> es una landing page experimental desarrollada para demostrar el manejo del código de programación <br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, CSS3, JavaScript, SASS y la libreria de React. <br/> <br/> La barra lateral de navegación permite un desplazamiento más fluido y rápido en las versiones de escritorio.</p>",
+    code: "https://github.com/SalvasDev/mntn",
+    demo: "https://mntn-sal.netlify.app/",
   },
 
   {
-    id: '102',
+    id: "102",
     area: "web",
     name: "Edie Homepage",
     designed: "Thu Nghiem",
     developed: "Salvador Sánchez",
     thumbnail: "/img/edie/thumb-edie.png",
-    pics:
-      [
-        '/img/edie/edie-tablet.png',
-        '/img/edie/edie-header.png',
-        '/img/edie/edie-services.png',
-        '/img/edie/edie-projects1.png',
-        '/img/edie/edie-projects2.png',
-        '/img/edie/edie-team.png',
-        '/img/edie/edie-footer.png'
-      ],
-    description: "<p><strong>Edie homepage</strong> es una landing page experimental desarrollada para demostrar el manejo del código de programación.<br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, CSS3 y JavaScript. <br/> <br/> Esta landing page muestra el manejo del diseño responsivo.</p>",
-    code: 'https://github.com/SalvasDev/edie-homepage',
-    demo: 'https://edie-homepage-salvasdev.netlify.app/'
+    pics: [
+      "/img/edie/edie-tablet.png",
+      "/img/edie/edie-header.png",
+      "/img/edie/edie-services.png",
+      "/img/edie/edie-projects1.png",
+      "/img/edie/edie-projects2.png",
+      "/img/edie/edie-team.png",
+      "/img/edie/edie-footer.png",
+    ],
+    description:
+      "<p><strong>Edie homepage</strong> es una landing page experimental desarrollada para demostrar el manejo del código de programación.<br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, CSS3 y JavaScript. <br/> <br/> Esta landing page muestra el manejo del diseño responsivo.</p>",
+    code: "https://github.com/SalvasDev/edie-homepage",
+    demo: "https://edie-homepage-salvasdev.netlify.app/",
   },
 
   {
-    id: '103',
+    id: "103",
     area: "web",
     name: "Analytics",
     designed: "Figma community",
     developed: "Salvador Sánchez",
     thumbnail: "/img/analytics/thumb-analytics.png",
-    pics:
-      [
-        '/img/analytics/lapandmov.png',
-        '/img/analytics/mob-hand.png',
-        '/img/analytics/tablet-hands.png',
-        '/img/analytics/mob-hand2.png',
-        '/img/analytics/analytics-deskhome2.png',
-      ],
-    description: "<p><strong>Analytics</strong> es una landing page experimental desarrollada para demostrar el manejo del código de programación.<br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, CSS modules, TypeScript, React. <br/> <br/> Esta landing page muestra el manejo del diseño responsivo.</p>",
-    code: 'https://github.com/SalvasDev/analytics',
-    demo: 'https://analytics-sal.netlify.app/'
+    pics: [
+      "/img/analytics/lapandmov.png",
+      "/img/analytics/mob-hand.png",
+      "/img/analytics/tablet-hands.png",
+      "/img/analytics/mob-hand2.png",
+      "/img/analytics/analytics-deskhome2.png",
+    ],
+    description:
+      "<p><strong>Analytics</strong> es una landing page experimental desarrollada para demostrar el manejo del código de programación.<br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, CSS modules, TypeScript, React. <br/> <br/> Esta landing page muestra el manejo del diseño responsivo.</p>",
+    code: "https://github.com/SalvasDev/analytics",
+    demo: "https://analytics-sal.netlify.app/",
   },
   {
-    id: '104',
+    id: "104",
     area: "web",
     name: "Vitalinea",
     designed: "IA Interactive",
     developed: "Salvador Sánchez",
     thumbnail: "/img/vitalinea/thumb-vitalinea.png",
-    pics:
-      [
-        '/img/vitalinea/vitalinea-1.png',
-        '/img/vitalinea/vitalinea-2.png',
-        '/img/vitalinea/vitalinea-3.png'
-      ],
-    description: "<p><strong>Vitalinea</strong> es una landing page experimental como reto de programación, desarrollada para demostrar el manejo del código de programación.<br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, SASS, JavaScript, React. <br/> <br/> Esta landing page muestra el manejo del diseño responsivo.</p>",
-    code: 'https://github.com/SalvasDev/vitalinea',
-    demo: 'https://vitalinea-salvador.netlify.app'
+    pics: [
+      "/img/vitalinea/vitalinea-1.png",
+      "/img/vitalinea/vitalinea-2.png",
+      "/img/vitalinea/vitalinea-3.png",
+    ],
+    description:
+      "<p><strong>Vitalinea</strong> es una landing page experimental como reto de programación, desarrollada para demostrar el manejo del código de programación.<br/> <br/> Para su realiazación se utilizaron diferentes tecnologías como: HTML5, SASS, JavaScript, React. <br/> <br/> Esta landing page muestra el manejo del diseño responsivo.</p>",
+    code: "https://github.com/SalvasDev/vitalinea",
+    demo: "https://vitalinea-salvador.netlify.app",
   },
 
   {
-    id: '105',
+    id: "105",
     area: "web",
     name: "Remo jobs",
     designed: "Thu Nghiem",
     developed: "Salvador Sánchez",
     thumbnail: "/img/remo-jobs/thumb-remo-jobs2.png",
-    pics:
-      [
-        '/img/remo-jobs/remo-jobs-1.png',
-        '/img/remo-jobs/remo-jobs-2.png',
-        '/img/remo-jobs/remo-jobs-3.png'
-      ],
-    description: "<p><strong>Remo jobs</strong> es una aplicación web para buscar empleos en la industria de TI.<br/> <br/>Para su realización utilicé diversas tecnologías, como JavaScript con la biblioteca React, componentes de estilo CSS3 y JSX para crear la interfaz de usuario. La gestión de versiones se realizó con Git y Github, y la aplicación se implementó en Netlify.</p>",
-    code: 'https://github.com/SalvasDev/remo-jobs',
-    demo: 'https://remo-jobs.netlify.app/'
+    pics: [
+      "/img/remo-jobs/remo-jobs-1.png",
+      "/img/remo-jobs/remo-jobs-2.png",
+      "/img/remo-jobs/remo-jobs-3.png",
+    ],
+    description:
+      "<p><strong>Remo jobs</strong> es una aplicación web para buscar empleos en la industria de TI.<br/> <br/>Para su realización utilicé diversas tecnologías, como JavaScript con la biblioteca React, componentes de estilo CSS3 y JSX para crear la interfaz de usuario. La gestión de versiones se realizó con Git y Github, y la aplicación se implementó en Netlify.</p>",
+    code: "https://github.com/SalvasDev/remo-jobs",
+    demo: "https://remo-jobs.netlify.app/",
   },
   {
-    id: '106',
+    id: "106",
     area: "web",
     name: "Weather app",
     designed: "Thu Nghiem",
     developed: "Salvador Sánchez",
     thumbnail: "/img/weather-app/thumb-weather-app.png",
-    pics:
-      [
-        '/img/weather-app/weather-app-1.png'
-
-      ],
-    description: "<p><strong>Weather app</strong> es una aplicación para consultar el clima en muchas ciudades del mundo. Esta aplicación utiliza varias consultas de API. <br/> <br/>Para su realización utilicé diversas tecnologías, como JavaScript con la biblioteca React, componentes de estilo CSS3 y JSX para crear la interfaz de usuario. La gestión de versiones se realizó con Git y Github, y la aplicación se implementó en Netlify.</p>",
-    code: 'https://github.com/SalvasDev/weather-app',
-    demo: 'https://weather-app-sal.netlify.app/'
+    pics: ["/img/weather-app/weather-app-1.png"],
+    description:
+      "<p><strong>Weather app</strong> es una aplicación para consultar el clima en muchas ciudades del mundo. Esta aplicación utiliza varias consultas de API. <br/> <br/>Para su realización utilicé diversas tecnologías, como JavaScript con la biblioteca React, componentes de estilo CSS3 y JSX para crear la interfaz de usuario. La gestión de versiones se realizó con Git y Github, y la aplicación se implementó en Netlify.</p>",
+    code: "https://github.com/SalvasDev/weather-app",
+    demo: "https://weather-app-sal.netlify.app/",
   },
   {
-    id: '107',
+    id: "107",
     area: "web",
     name: "Pizzas Don Remolo",
     designed: "Paola Rivero y Salvador Sánchez",
     developed: "Salvador Sánchez",
     thumbnail: "/img/don-remolo/thumb-don-remolo.png",
-    pics:
-      [
-        '/img/don-remolo/loading.png',
-        '/img/don-remolo/menu.png',
-        '/img/don-remolo/pizzas.png',
-        '/img/don-remolo/cart.png',
-
-      ],
-    description: "<p><strong>Don Remolo Pizzas</strong> es una aplicación web para vender pizzas y otros alimentos.<br/> <br/>Para su realización utilizamos diversas tecnologías como: Next.js, Material UI, Node y Mongodbon. La gestión de versiones se realizó con Git y Github.</p>",
-    code: 'https://github.com/SalvasDev/Pizzas-Don-Remolo',
-    demo: 'https://remolo-pizzas.vercel.app/'
+    pics: [
+      "/img/don-remolo/loading.png",
+      "/img/don-remolo/menu.png",
+      "/img/don-remolo/pizzas.png",
+      "/img/don-remolo/cart.png",
+    ],
+    description:
+      "<p><strong>Don Remolo Pizzas</strong> es una aplicación web para vender pizzas y otros alimentos.<br/> <br/>Para su realización utilizamos diversas tecnologías como: Next.js, Material UI, Node y Mongodbon. La gestión de versiones se realizó con Git y Github.</p>",
+    code: "https://github.com/SalvasDev/Pizzas-Don-Remolo",
+    demo: "https://remolo-pizzas.vercel.app/",
   },
-  {
-    id: '108',
-    area: "web",
-    name: "Pokémon app",
-    designed: "Fernando Herrera",
-    developed: "Salvador Sánchez",
-    thumbnail: "/img/pokemon/thumb-pokemon.png",
-    pics:
-      [
-        '/img/pokemon/pokemon-home.png',
-        '/img/pokemon/pokemon-detail.png',
-        '/img/pokemon/pokemon-favorites.png',
+  // {
+  //   id: '108',
+  //   area: "web",
+  //   name: "Pokémon app",
+  //   designed: "Fernando Herrera",
+  //   developed: "Salvador Sánchez",
+  //   thumbnail: "/img/pokemon/thumb-pokemon.png",
+  //   pics:
+  //     [
+  //       '/img/pokemon/pokemon-home.png',
+  //       '/img/pokemon/pokemon-detail.png',
+  //       '/img/pokemon/pokemon-favorites.png',
 
-      ],
-    description: "<p><strong>Pokemon App</strong> es una aplicación web que extrae pokemons de una api y los muestra con un renderizado estatico de lado del servidor para servir la información en tiempo de producción.<br/> <br/>Para su realización utilizamos diversas tecnologías como: Next.js y Next UI. La gestión de versiones se realizó con Git y Github.</p>",
-    code: 'https://github.com/SalvasDev/02-pokemon-static',
-    demo: 'https://02-pokemon-static-sigma.vercel.app/'
-  },
+  //     ],
+  //   description: "<p><strong>Pokemon App</strong> es una aplicación web que extrae pokemons de una api y los muestra con un renderizado estatico de lado del servidor para servir la información en tiempo de producción.<br/> <br/>Para su realización utilizamos diversas tecnologías como: Next.js y Next UI. La gestión de versiones se realizó con Git y Github.</p>",
+  //   code: 'https://github.com/SalvasDev/02-pokemon-static',
+  //   demo: 'https://02-pokemon-static-sigma.vercel.app/'
+  // },
   {
-    id: '109',
+    id: "109",
     area: "web",
     name: "OpenJira app",
     designed: "Fernando Herrera",
     developed: "Salvador Sánchez",
     thumbnail: "/img/open-jira/thumb-openjira.png",
-    pics:
-      [
-        '/img/open-jira/openjira-home.png',
-        '/img/open-jira/openjira-edition.png',
-      ],
-    description: "<p><strong>OpenJira app</strong> es una aplicación web que nos permite agregar, editar y eliminar tareas así como modificar su status modificando su etiqueta o arrastrando a la columna deseada.<br/> <br/>Para su realización utilizamos diversas tecnologías como: Next.js y Material UI para el front-end y Nodejs y Mongodb para el backend. La gestión de versiones se realizó con Git y Github.</p>",
-    code: 'https://github.com/SalvasDev/03-open-jira',
-    demo: 'https://03-open-jira-taupe.vercel.app/'
+    pics: [
+      "/img/open-jira/openjira-home.png",
+      "/img/open-jira/openjira-edition.png",
+    ],
+    description:
+      "<p><strong>OpenJira app</strong> es una aplicación web que nos permite agregar, editar y eliminar tareas así como modificar su status modificando su etiqueta o arrastrando a la columna deseada.<br/> <br/>Para su realización utilizamos diversas tecnologías como: Next.js y Material UI para el front-end y Nodejs y Mongodb para el backend. La gestión de versiones se realizó con Git y Github.</p>",
+    code: "https://github.com/SalvasDev/03-open-jira",
+    demo: "https://03-open-jira-taupe.vercel.app/",
   },
-
 ];
